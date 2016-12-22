31ea03c831bc234c750bc47d76565149808da2e5
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2016-12-22 10:08:40 -0500

    updates

--- simulate.c
+++ simulate.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include "nessemble.h"
 
 // https://github.com/bfirsh/jsnes/blob/master/source/cpu.js
@@ -17,6 +17,10 @@ struct regs {
 struct regs registers = { 0, 0, 0, 0x8000, 0xFF, { 0, 0, 0, 0, 0, 0, 0 } };
 
 char *rom_data = NULL;
+
+#define BUF_SIZE 256
+
+char buffer[BUF_SIZE];
 
 int simulate(char *input) {
     int rc = RETURN_OK, i = 0, l = 0, header = 0;
@@ -70,44 +74,49 @@ int simulate(char *input) {
 
     // simulate
     while (1) {
-        opcode_index = (int)rom_data[registers.pc] & 0xFF;
-        length = opcodes[opcode_index].length;
-
-        printf(" PC  AC XR YR SP NV-BDIZC\n");
-        printf("%04X ", registers.pc);
-        printf("%02X ", registers.a);
-        printf("%02X ", registers.x);
-        printf("%02X ", registers.y);
-        printf("%02X ", registers.sp);
-
-        printf("%d", registers.flags.negative & 1);
-        printf("%d0", registers.flags.overflow & 1);
-        printf("%d", registers.flags.brk & 1);
-        printf("%d", registers.flags.decimal & 1);
-        printf("%d", registers.flags.ineterrupt & 1);
-        printf("%d", registers.flags.zero & 1);
-        printf("%d", registers.flags.carry & 1);
-
-        printf("\n\n");
-
-        if (length == 1) {
-            (*opcodes[opcode_index].func)(opcode_index, 0);
-        } else {
-            arg0 = (int)rom_data[registers.pc+1] & 0xFF;
-
-            if (length == 2) {
-                (*opcodes[opcode_index].func)(opcode_index, arg0);
-            } else if (length == 3) {
-                arg1 = (int)rom_data[registers.pc+2] & 0xFF;
-                (*opcodes[opcode_index].func)(opcode_index, (arg1 << 8) | arg0);
-            }
-        }
-
         if (registers.pc == -1) {
             break;
         }
 
-        getchar();
+        printf("nessemble> ");
+
+        fgets(buffer, BUF_SIZE, stdin);
+
+        if (strncmp(buffer, ".registers", 10) == 0) {
+            print_registers();
+        }
+
+        if (strncmp(buffer, ".instruction", 12) == 0) {
+            print_instruction();
+        }
+
+        if (strncmp(buffer, ".memory", 7) == 0) {
+            print_memory(buffer+8);
+        }
+
+        if (strncmp(buffer, ".goto", 5) == 0) {
+            print_goto(buffer+6);
+        }
+
+        if (strncmp(buffer, ".step", 5) == 0) {
+            print_instruction();
+
+            if (step()) {
+                print_registers();
+                break;
+            }
+
+            print_registers();
+        }
+
+        if (strncmp(buffer, ".run", 4) == 0) {
+            while (1) {
+                if (step()) {
+                    print_registers();
+                    break;
+                }
+            }
+        }
     }
 
 cleanup:
@@ -116,6 +125,155 @@ cleanup:
     }
 
     return rc;
+}
+
+void print_registers() {
+    printf(" PC  AC XR YR SP NV-BDIZC\n");
+    printf("%04X ", registers.pc);
+    printf("%02X ", registers.a);
+    printf("%02X ", registers.x);
+    printf("%02X ", registers.y);
+    printf("%02X ", registers.sp);
+
+    printf("%d", registers.flags.negative & 1);
+    printf("%d0", registers.flags.overflow & 1);
+    printf("%d", registers.flags.brk & 1);
+    printf("%d", registers.flags.decimal & 1);
+    printf("%d", registers.flags.ineterrupt & 1);
+    printf("%d", registers.flags.zero & 1);
+    printf("%d", registers.flags.carry & 1);
+
+    printf("\n\n");
+}
+
+void print_instruction() {
+    int opcode_index = 0, mode = 0, arg0 = 0, arg1 = 0;
+    char *mnemonic = NULL;
+
+    opcode_index = (int)rom_data[registers.pc] & 0xFF;
+    mode = opcodes[opcode_index].mode;
+    mnemonic = opcodes[opcode_index].mnemonic;
+
+    switch (mode) {
+    case MODE_IMPLIED:
+        printf("%s\n\n", mnemonic);
+        break;
+    case MODE_ACCUMULATOR:
+        fprintf("%s A\n\n", mnemonic);
+        break;
+    case MODE_RELATIVE:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        if (arg0 >= 0x80) {
+            printf("%s $%04X\n\n", mnemonic, registers.pc - (0xFF - arg0 - 1));
+        } else {
+            printf("%s $%04X\n\n", mnemonic, registers.pc + arg0);
+        }
+        break;
+    case MODE_IMMEDIATE:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        printf("%s #$%02X\n\n", mnemonic, arg0);
+        break;
+    case MODE_ZEROPAGE:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        printf("%s <$%02X\n\n", mnemonic, arg0);
+        break;
+    case MODE_ZEROPAGE_X:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        printf("%s <$%02X, X\n\n", mnemonic, arg0);
+        break;
+    case MODE_ZEROPAGE_Y:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        printf("%s <$%02X, Y\n\n", mnemonic, arg0);
+        break;
+    case MODE_ABSOLUTE:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        arg1 = (int)rom_data[registers.pc+2] & 0xFF;
+        printf("%s $%04X\n\n", mnemonic, (arg1 << 8) | arg0);
+        break;
+    case MODE_ABSOLUTE_X:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        arg1 = (int)rom_data[registers.pc+2] & 0xFF;
+        printf("%s $%04X, X\n\n", mnemonic, (arg1 << 8) | arg0);
+        break;
+    case MODE_ABSOLUTE_Y:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        arg1 = (int)rom_data[registers.pc+2] & 0xFF;
+        printf("%s $%04X, Y\n\n", mnemonic, (arg1 << 8) | arg0);
+        break;
+    case MODE_INDIRECT:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        arg1 = (int)rom_data[registers.pc+2] & 0xFF;
+        printf("%s ($%04X)\n\n", mnemonic, (arg1 << 8) | arg0);
+        break;
+    case MODE_INDIRECT_X:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        printf("%s ($%02X, X)\n\n", mnemonic, arg0);
+        break;
+    case MODE_INDIRECT_Y:
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+        printf("%s ($%02X), Y\n\n", mnemonic, arg0);
+        break;
+    default:
+        break;
+    }
+}
+
+int step() {
+    int rc = RETURN_OK, opcode_index = 0, length = 0, arg0 = 0, arg1 = 0;
+
+    opcode_index = (int)rom_data[registers.pc] & 0xFF;
+    length = opcodes[opcode_index].length;
+
+    if (!is_flag_undocumented() && (opcodes[opcode_index].meta & META_UNDOCUMENTED) != 0) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
+    if (length == 1) {
+        (*opcodes[opcode_index].func)(opcode_index, 0);
+    } else {
+        arg0 = (int)rom_data[registers.pc+1] & 0xFF;
+
+        if (length == 2) {
+            (*opcodes[opcode_index].func)(opcode_index, arg0);
+        } else if (length == 3) {
+            arg1 = (int)rom_data[registers.pc+2] & 0xFF;
+            (*opcodes[opcode_index].func)(opcode_index, (arg1 << 8) | arg0);
+        }
+    }
+
+cleanup:
+    return rc;
+}
+
+void print_memory(char *input) {
+    int a = 0, b = 0, i = 0, l = 0;
+    size_t length = 0;
+
+    length = strlen(input);
+
+    if (length <= 5) {
+        a = (int)strtol(input, NULL, 16);
+        b = a + 1;
+    } else if (length <= 10) {
+        b = (int)strtol(input+5, NULL, 16);
+        input[5] = '\0';
+        a = (int)strtol(input, NULL, 16);
+
+        b++;
+    }
+
+    for (i = a, l = b; i < l; i++) {
+        printf("%02X ", (int)rom_data[i] & 0xFF);
+    }
+
+    printf("\n");
+}
+
+void print_goto(char *input) {
+    input[4] = '\0';
+
+    registers.pc = (int)strtol(input, NULL, 16);
 }
 
 int do_aac(int opcode_index, int value) {
@@ -388,11 +546,9 @@ int do_sre(int opcode_index, int value) {
 }
 
 int do_sta(int opcode_index, int value) {
-    int length = 0;
+    int length = opcodes[opcode_index].length;
 
-    length = opcodes[opcode_index].length;
-
-    rom_data[registers.a] = (char)value;
+    rom_data[value] = (char)registers.a;
 
     registers.pc += length;
 }
