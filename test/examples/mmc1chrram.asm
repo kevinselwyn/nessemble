5c4e087b5770151f7f239a8335b9423cea3de539
Kevin Selwyn <kevinselwyn@gmail.com>
2016-12-26 10:06:58 -0500

    more tests

---
+++ test/examples/mmc1chrram.asm
@@ -0,0 +1,28 @@
+.inesprg 8
+.ineschr 0
+.inesmap 1
+.inesmir 0
+
+.prg0
+    .db $01, $02
+
+.prg1
+    .db $03, $04
+
+.prg2
+    .db $05, $06
+
+.prg3
+    .db $07, $08
+
+.prg4
+    .db $09, $0A
+
+.prg5
+    .db $0B, $0C
+
+.prg6
+    .db $0D, $0E
+
+.prg7
+    .db $0F, $10
