c5974226a937b255dc012804cecf1dfd201719c5
Kevin Selwyn <kevinselwyn@gmail.com>
2016-12-27 10:10:38 -0500

    check stuff

--- check_utils.c
+++ check_utils.c
@@ -1,6 +1,21 @@
 #include <check.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "nessemble.h"
+
+int saved_stderr = 0;
+char buffer[4096] = { 0 };
+
+void stderr_fake() {
+    saved_stderr = dup(STDERR_FILENO);
+    fclose(stderr);
+    stderr = fmemopen(buffer, sizeof(buffer), "w");
+    setbuf(stderr, NULL);
+}
+
+void stderr_real() {
+    dup2(saved_stderr, STDERR_FILENO);
+}
 
 START_TEST(test_utils_power) {
     ck_assert_int_eq(power(1, 1), 1);
@@ -47,6 +62,97 @@ START_TEST(test_utils_load_file) {
 }
 END_TEST
 
+START_TEST(test_utils_is_flag_undocumented) {
+    int old_flags = flags;
+
+    flags = 0;
+    ck_assert_int_eq(is_flag_undocumented(), 0);
+
+    flags |= FLAG_UNDOCUMENTED;
+    ck_assert_int_eq(is_flag_undocumented(), 1);
+
+    flags = old_flags;
+}
+END_TEST
+
+START_TEST(test_utils_is_flag_nes) {
+    int old_flags = flags;
+
+    flags = 0;
+    ck_assert_int_eq(is_flag_nes(), 0);
+
+    flags |= FLAG_NES;
+    ck_assert_int_eq(is_flag_nes(), 1);
+
+    flags = old_flags;
+}
+END_TEST
+
+START_TEST(test_utils_is_flag_disassemble) {
+    int old_flags = flags;
+
+    flags = 0;
+    ck_assert_int_eq(is_flag_disassemble(), 0);
+
+    flags |= FLAG_DISASSEMBLE;
+    ck_assert_int_eq(is_flag_disassemble(), 1);
+
+    flags = old_flags;
+}
+END_TEST
+
+START_TEST(test_utils_is_flag_simulate) {
+    int old_flags = flags;
+
+    flags = 0;
+    ck_assert_int_eq(is_flag_simulate(), 0);
+
+    flags |= FLAG_SIMULATE;
+    ck_assert_int_eq(is_flag_simulate(), 1);
+
+    flags = old_flags;
+}
+END_TEST
+
+START_TEST(test_utils_is_flag_check) {
+    int old_flags = flags;
+
+    flags = 0;
+    ck_assert_int_eq(is_flag_check(), 0);
+
+    flags |= FLAG_CHECK;
+    ck_assert_int_eq(is_flag_check(), 1);
+
+    flags = old_flags;
+}
+END_TEST
+
+START_TEST(test_utils_is_segment_chr) {
+    int old_segment_type = segment_type;
+
+    segment_type = 0;
+    ck_assert_int_eq(is_segment_chr(), 0);
+
+    segment_type |= SEGMENT_CHR;
+    ck_assert_int_eq(is_segment_chr(), 1);
+
+    segment_type = old_segment_type;
+}
+END_TEST
+
+START_TEST(test_utils_is_segment_prg) {
+    int old_segment_type = segment_type;
+
+    segment_type = 0;
+    ck_assert_int_eq(is_segment_prg(), 0);
+
+    segment_type |= SEGMENT_PRG;
+    ck_assert_int_eq(is_segment_prg(), 1);
+
+    segment_type = old_segment_type;
+}
+END_TEST
+
 Suite * utils_suite(void) {
     Suite *s;
     TCase *tc_core;
@@ -60,6 +166,13 @@ Suite * utils_suite(void) {
     tcase_add_test(tc_core, test_utils_bin2int);
     tcase_add_test(tc_core, test_utils_dec2int);
     tcase_add_test(tc_core, test_utils_load_file);
+    tcase_add_test(tc_core, test_utils_is_flag_undocumented);
+    tcase_add_test(tc_core, test_utils_is_flag_nes);
+    tcase_add_test(tc_core, test_utils_is_flag_disassemble);
+    tcase_add_test(tc_core, test_utils_is_flag_simulate);
+    tcase_add_test(tc_core, test_utils_is_flag_check);
+    tcase_add_test(tc_core, test_utils_is_segment_chr);
+    tcase_add_test(tc_core, test_utils_is_segment_prg);
 
     suite_add_tcase(s, tc_core);
 
