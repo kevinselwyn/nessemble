d7b125e73cf78b571ec81ab0e033ebe891255855
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2016-12-26 20:04:10 -0500

    fixes

--- check.sh
+++ check.sh
@@ -2,7 +2,7 @@
 
 for file in check_*.c
 do
-    name=`echo "$file" | sed -r 's/check_//g' | sed -r 's/.c//g'`
+    name=`echo "$file" | sed -r 's/check_//g' | sed -r 's/\.c//g'`
     gcc -o suite_$name $file $name.c `pkg-config --cflags --libs check`
 done
 
