e6afad4dc8a45d17d445e093f473e0dba3c6cc0a
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2017-01-12 15:35:09 -0500

    incscreen start

---
+++ test/examples/incscreen/incscreen.asm
@@ -0,0 +1,6 @@
+;http://www.dustmop.io/blog/2015/04/28/nes-graphics-part-1/
+
+.incscreen "incscreen.png" "palette"
+.incscreen "incscreen.png" "attributes"
+.incscreen "incscreen.png" "chr"
+.incscreen "incscreen.png" "nametable"
