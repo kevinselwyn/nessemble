cb47cd77457d140122a3b1a30d66968c22e64b8c
Kevin Selwyn <kevinselwyn@gmail.com>
2017-01-21 22:58:46 -0500

    nerdy-nights audio

---
+++ test/nerdy-nights/square1/square1.asm
@@ -0,0 +1,41 @@
+.inesprg 2
+.ineschr 1
+.inesmap 0
+.inesmir 1
+
+;;;;;;;;;;;;;;;;
+
+.prg0
+
+RESET:
+    SEI
+    CLD
+
+    LDA #%00000001
+    STA $4015
+
+    LDA #%10111111
+    STA $4000
+
+    LDA #$C9
+    STA $4002
+    LDA #$00
+    STA $4003
+
+;;;;;;;;
+
+forever:
+    JMP forever
+
+;;;;;;;;
+
+NMI:
+    RTI
+
+;;;;;;;;
+
+.org $FFFA
+
+    .dw NMI
+    .dw RESET
+    .dw 0
