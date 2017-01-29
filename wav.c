068a5dee5d9e61d7ebafc077b7153db235a6dd0c
Kevin Selwyn <kevinselwyn@gmail.com>
2017-01-29 15:28:52 -0500

    Abstract wav

--- wav.c
+++ wav.c
@@ -1,22 +1,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "nessemble.h"
-
-typedef struct wave_fmt {
-    unsigned int format;
-    unsigned int channels;
-    long int sample_rate;
-    long int bytes_sec;
-    unsigned int frame_size;
-    unsigned int bits_sample;
-} wave_fmt;
-
-typedef struct wave_src {
-    wave_fmt fmt;
-    FILE *fp;
-    long int chunk_left;
-    int cur_chn;
-} wave_src;
+#include "wav.h"
 
 static unsigned int fgetu16(FILE *fp) {
     unsigned int a = (unsigned int)fgetc(fp);
@@ -56,7 +41,7 @@ cleanup:
     return rc;
 }
 
-static int wav_sample(wave_src *wav) {
+int wav_sample(wave_src *wav) {
     int i = 0, cur_sample = 0;
 
     if (wav->chunk_left == 0) {
@@ -84,7 +69,7 @@ static int wav_sample(wave_src *wav) {
     return cur_sample;
 }
 
-static unsigned int open_wav(wave_src *wav, char *filename) {
+unsigned int open_wav(wave_src *wav, char *filename) {
     unsigned int rc = RETURN_OK, got_fmt = FALSE;
     size_t length = 0;
     char buffer[256];
@@ -153,83 +138,8 @@ cleanup:
     return rc;
 }
 
-static void close_wav(wave_src *wav) {
+void close_wav(wave_src *wav) {
     if (wav->fp) {
         (void)fclose(wav->fp);
     }
-}
-
-void pseudo_incwav(char *string, int amplitude) {
-    int x = 0, y = 0;
-    int subsample = 99, oversample = 100;
-    long filelen = 0;
-    char *path = NULL;
-    wave_src wav = { { 0, 0, 0, 0, 0, 0 } , NULL, 0, 0 };
-
-    if (get_fullpath(&path, string) != 0) {
-        yyerror("Could not get full path of %s", string);
-        goto cleanup;
-    }
-
-    if (!path) {
-        yyerror("Could not get full path of %s", string);
-        goto cleanup;
-    }
-
-    if (open_wav(&wav, path) != 0) {
-        yyerror("Could not open %s", string);
-        goto cleanup;
-    }
-
-    if (wav.fmt.channels != 1) {
-        yyerror("%s is not mono", string);
-        goto cleanup;
-    }
-
-    if (amplitude < 2) {
-        amplitude = 2;
-    }
-
-    if (amplitude > 40) {
-        amplitude = 40;
-    }
-
-    while (wav.chunk_left > 0) {
-        unsigned int i = 0, code = 0;
-
-        for (i = 0; i < 8; i++) {
-            while (subsample < 100) {
-                x = (wav_sample(&wav) * amplitude + 16384) >> 15;
-                filelen--;
-                subsample += oversample;
-            }
-
-            subsample -= 100;
-
-            if (x >= y) {
-                y++;
-
-                if (y > 31) {
-                    y = 31;
-                }
-
-                code |= 1 << i;
-            } else {
-                y--;
-
-                if (y < -32) {
-                    y = -32;
-                }
-            }
-        }
-
-        write_byte(code);
-    }
-
-cleanup:
-    if (path) {
-        free(path);
-    }
-
-    close_wav(&wav);
 }
