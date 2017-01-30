a514a7b125583c5eaff597a3ce5b7ac5e495c193
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2017-01-30 11:32:44 -0500

    Rearrange macro

--- macro.c
+++ macro.c
@@ -47,17 +47,3 @@ int get_macro(char *name) {
 
     return index;
 }
-
-void pseudo_macro(char *string) {
-    int index = get_macro(string);
-
-    if (index == -1) {
-        error_add("Macro `%s` was not defined", string);
-        goto cleanup;
-    }
-
-    include_string_push(macros[index].text);
-
-cleanup:
-    return;
-}
---
+++ pseudo/macro.c
@@ -0,0 +1,19 @@
+#include "../nessemble.h"
+
+/**
+ * .macro pseudo instruction
+ * @param {char *} string - Macro string name
+ */
+ void pseudo_macro(char *string) {
+     int index = get_macro(string);
+
+     if (index == -1) {
+         error_add("Macro `%s` was not defined", string);
+         goto cleanup;
+     }
+
+     include_string_push(macros[index].text);
+
+ cleanup:
+     return;
+ }
