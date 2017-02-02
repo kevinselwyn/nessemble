ac78a656ed57b8fd690715c3e553062e60980eb9
Kevin Selwyn <kevinselwyn@gmail.com>
2017-02-02 11:08:43 -0500

    midi progress

--- midi.h
+++ midi.h
@@ -9,9 +9,20 @@ enum {
 
 struct midi_data read_midi(char *filename);
 
+struct midi_event {
+    unsigned int type;
+};
+
+struct midi_track {
+    char *header[4], *data;
+    unsigned int length;
+    struct midi_event *events;
+};
+
 struct midi_data {
     char *header[4];
-    unsigned int header_length, format, tracks, quarter;
+    unsigned int header_length, format, track_count, quarter;
+    struct midi_track *tracks;
 };
 
 #endif /* _MIDI_H */
