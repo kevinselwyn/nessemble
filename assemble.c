d03bc3b54c836c77c0d0173bfe9d00d05a623a13
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2017-02-06 22:17:57 -0500

    Overflow improvements

--- assemble.c
+++ assemble.c
@@ -98,20 +98,12 @@ void write_byte(unsigned int byte) {
     if (is_segment_prg() == TRUE) {
         if (pass == 1) {
             if (ines.prg < 2) {
-                if (prg_offsets[prg_index] >= (BANK_PRG * 2)) {
+                if (prg_offsets[prg_index] >= BANK_PRG) {
                     warning("Overflowing PRG Bank %d", prg_index);
                 }
             } else {
-                if (prg_offsets[prg_index] >= BANK_PRG) {
-                    if (prg_index == 0) {
-                        if (prg_offsets[prg_index] >= (BANK_PRG * 2)) {
-                            warning("Overflowing PRG Bank %d", prg_index);
-                        }
-                    } else {
-                        if (prg_offsets[prg_index] >= BANK_PRG) {
-                            warning("Overflowing PRG Bank %d", prg_index);
-                        }
-                    }
+                if (prg_offsets[prg_index] >= BANK_PRG * ((prg_index % 2) + 1)) {
+                    warning("Overflowing PRG Bank %d", prg_index);
                 }
             }
         }
