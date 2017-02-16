4f7e17b19b2f05b29e95c30e74b636b4a8252698
Kevin Selwyn <kevinselwyn@gmail.com>
2017-02-16 12:10:47 -0500

    request fix

--- download.c
+++ download.c
@@ -22,7 +22,7 @@ static size_t write_response(void *ptr, size_t size, size_t nmemb, void *stream)
 
 unsigned int get_request(char **request, size_t *request_length, char *url, char *mime_type) {
     unsigned int rc = RETURN_OK;
-    long code = 0;
+    unsigned int code = 0;
     size_t mime_type_length = 0;
     char *data = NULL, *content_type = NULL;
     struct curl_slist *headers = NULL;
@@ -30,21 +30,21 @@ unsigned int get_request(char **request, size_t *request_length, char *url, char
     CURLcode status;
 
     if (curl_global_init(CURL_GLOBAL_ALL) != 0) {
-        rc = RETURN_EPERM;
+        code = 503;
         goto cleanup;
     }
 
     curl = curl_easy_init();
 
     if (!curl) {
-        rc = RETURN_EPERM;
+        code = 503;
         goto cleanup;
     }
 
     data = (char *)malloc(sizeof(char) * BUFFER_SIZE);
 
     if (!data) {
-        rc = RETURN_EPERM;
+        code = 503;
         goto cleanup;
     }
 
@@ -59,7 +59,7 @@ unsigned int get_request(char **request, size_t *request_length, char *url, char
     content_type = (char *)malloc(sizeof(char) * (mime_type_length + 15));
 
     if (!content_type) {
-        rc = RETURN_EPERM;
+        code = 503;
         goto cleanup;
     }
 
@@ -74,16 +74,13 @@ unsigned int get_request(char **request, size_t *request_length, char *url, char
     status = curl_easy_perform(curl);
 
     if (status != 0) {
-        fprintf(stderr, "Could not reach the registry\n");
-
-        rc = RETURN_EPERM;
+        code = 503;
         goto cleanup;
     }
 
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &code);
 
     if (code != 200) {
-        rc = RETURN_EPERM;
         goto cleanup;
     }
 
@@ -101,5 +98,5 @@ cleanup:
         free(content_type);
     }
 
-    return rc;
+    return code;
 }
