59307824e90fa242125bf8fbf71027d048d33460
Kevin Selwyn <kevinselwyn@gmail.com>
2017-02-19 12:48:24 -0500

    bank symbol

--- test/examples/bank.asm
+++ test/examples/bank.asm
@@ -1,15 +1,17 @@
-.inesprg 1
+.inesprg 2
 .ineschr 1
 .inesmap 0
 .inesmir 0
 
 .prg 0
-label:
-    .db $01, $02
-    JMP label
+label1:
+    JMP label2
 
+.prg 1
 .org $FFFA
-    .db $03, $04, $05, $06, $07, $08
+
+label2:
+    JMP label1
 
 .chr 0
-    .db $09, $0A
+    .db $12, $34
