abfd579eee0c8bc5d70ca7c91e724331f07ac057
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2017-03-06 10:46:10 -0500

    Testing tweaks

---
+++ test/opcodes/and/and.asm
@@ -0,0 +1,30 @@
+    AND #$44
+    AND <$44
+    AND <$44, X
+    AND $4400
+    AND $4400, X
+    AND $4400, Y
+    AND [$44, X]
+    AND [$44], Y
+
+label:
+    AND #label
+    AND <label
+    AND <label, X
+    AND label
+    AND label, X
+    AND label, Y
+    AND [label, X]
+    AND [label], Y
+
+CONSTANT_8 = $44
+CONSTANT_16 = $4400
+
+    AND #CONSTANT_8
+    AND <CONSTANT_8
+    AND <CONSTANT_8, X
+    AND CONSTANT_16
+    AND CONSTANT_16, X
+    AND CONSTANT_16, Y
+    AND [CONSTANT_8, X]
+    AND [CONSTANT_8], Y
---
+++ test/opcodes/bpl/bpl.asm
@@ -0,0 +1,4 @@
+    BPL $00
+
+label:
+    BPL label
