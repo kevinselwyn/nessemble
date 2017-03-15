eea12f1036ef709f2b01fb38629b90b58008d651
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2017-03-14 23:03:55 -0400

    Consolidate repetitive code

--- pseudo/incrle.c
+++ pseudo/incrle.c
@@ -9,9 +9,8 @@ void pseudo_incrle(char *string) {
     unsigned int i = 0, j = 0, l = 0;
     unsigned int byte = 0, last = 0, count = 0;
     unsigned int run[256], has_run = FALSE, run_index = 0;
-    size_t bin_length = 0;
+    unsigned int bin_length = 0;
     char *path = NULL, *bin_data = NULL;
-    FILE *incrle = NULL;
 
     if (get_fullpath(&path, string) != 0) {
         yyerror("Could not get full path of %s", string);
@@ -23,33 +22,8 @@ void pseudo_incrle(char *string) {
         goto cleanup;
     }
 
-    incrle = fopen(path, "r");
-
-    if (!incrle) {
-        yyerror("Could not include %s", string);
-        goto cleanup;
-    }
-
-    if (fseek(incrle, 0, SEEK_END) != 0) {
-        yyerror("Seek error");
-        goto cleanup;
-    }
-
-    bin_length = (size_t)ftell(incrle);
-
-    if (fseek(incrle, 0, SEEK_SET) != 0) {
-        yyerror("Seek error");
-        goto cleanup;
-    }
-
-    if (bin_length == 0) {
-        goto cleanup;
-    }
-
-    bin_data = (char *)nessemble_malloc(sizeof(char) * (bin_length + 1));
-
-    if (fread(bin_data, 1, bin_length, incrle) != bin_length) {
-        yyerror("Could not read %s", string);
+    if (load_file(&bin_data, &bin_length, path) != RETURN_OK) {
+        yyerror("Could not read `%s`", path);
         goto cleanup;
     }
 
@@ -127,5 +101,4 @@ void pseudo_incrle(char *string) {
 cleanup:
     nessemble_free(path);
     nessemble_free(bin_data);
-    nessemble_fclose(incrle);
 }
