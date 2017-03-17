c649d6a03fa79c99fffff3a2cad40427e20e380b
Kevin Selwyn <kevinselwyn@gmail.com>
2017-03-16 22:39:10 -0400

    Finished tests

---
+++ test/examples/ines/test.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# coding=utf-8
+# pylint: disable=C0103,C0301
+"""ines test"""
+
+import os
+import subprocess
+
+def main():
+    """Main function"""
+
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    example = 'ines'
+
+    child = subprocess.Popen(['python', '%s/../examples-test.py' % (dirname), example], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    data = child.communicate()[0]
+    rc = child.returncode
+
+    if rc != 0:
+        print data
+
+    exit(rc)
+
+if __name__ == '__main__':
+    main()
