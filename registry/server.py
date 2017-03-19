e4307092ea9b46efc21128997768a35cf145bca3
Kevin Selwyn <kevinselwyn@gmail.com>
2017-03-19 19:51:52 -0400

    Reference endpoint

--- registry/server.py
+++ registry/server.py
@@ -477,6 +477,48 @@ def user_logout():
 
     return registry_response({})
 
+@app.route('/reference', methods=['GET'])
+@app.route('/reference/<path:path>', methods=['GET'])
+def reference(path=''):
+    paths = path.split('/')
+    output = ''
+    sql = []
+    sql_and = []
+    sql_data = {}
+
+    session = Session()
+
+    if not len(paths[0]):
+        result = session.execute('SELECT * FROM reference WHERE parent_id = :parent_id', {
+            'parent_id': 0
+        })
+
+        for row in result:
+            output += '  %s\n' % (row[2])
+    else:
+        sql.append('SELECT r%d.* FROM reference as r1' % (len(paths) + 1))
+
+        for i in range(0, len(paths)):
+            sql.append('INNER JOIN reference AS r%d on r%d.parent_id = r%d.id' % (i + 2, i + 2, i + 1))
+
+        for i in range(0, len(paths)):
+            sql_and.append('r%d.term = :term%d' % (i + 1, i + 1))
+            sql_data['term%d' % (i + 1)] = paths[i]
+
+        sql.append('WHERE %s' % (' AND '.join(sql_and)))
+        result = session.execute(' '.join(sql), sql_data)
+
+        for row in result:
+            if not row[2]:
+                output = row[3]
+            else:
+                output += '  %s\n' % (row[2])
+
+    if not len(output):
+        abort(404)
+
+    return registry_response(output, mimetype='text/plain')
+
 #----------------#
 # Import/Export
 
