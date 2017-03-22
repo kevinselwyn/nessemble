f75b9eb6338864649eb34fceb3d9763fb4085243
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2017-03-22 16:47:52 -0400

    Start of Windows support

--- i18n.c
+++ i18n.c
@@ -7,34 +7,60 @@
 #ifndef IS_WINDOWS
 #include <libintl.h>
 #include <locale.h>
+#else /* IS_WINDOWS */
+#include <windows.h>
 #endif /* IS_WINDOWS */
 
 static char *translated;
+static char *translated_path;
 
 void translate_init() {
     translated = (char *)nessemble_malloc(sizeof(char) * 256);
 
 #ifndef IS_WINDOWS
-    char *path = NULL;
-
-    if (get_home_path(&path, 2, "." PROGRAM_NAME, "locale") != RETURN_OK) {
+    if (get_home_path(&translated_path, 2, "." PROGRAM_NAME, "locale") != RETURN_OK) {
         return;
     }
 
     setlocale(LC_ALL, "");
-    bindtextdomain(PROGRAM_NAME, path);
+    bindtextdomain(PROGRAM_NAME, translated_path);
     textdomain(PROGRAM_NAME);
 
     nessemble_free(path);
+#else /* IS_WINDOWS */
+    char lang[16];
+
+    GetLocaleInfo(LOCALE_USER_DEFAULT, LOCALE_SISO639LANGNAME, &lang[0], 16);
+
+    if (get_home_path(&translated_path, 5, "." PROGRAM_NAME, "locale", lang, "LC_MESSAGES", PROGRAM_NAME ".mo") != RETURN_OK) {
+        return;
+    }
+
+    fprintf(stderr, "%s\n", translated_path);
 #endif /* IS_WINDOWS */
 }
 
 void translate_free() {
     nessemble_free(translated);
+    nessemble_free(translated_path);
 }
 
 char *translate(char *id) {
 #ifdef IS_WINDOWS
+    unsigned int string_count = 0;
+    FILE *translate_file = NULL;
+    unsigned int (*fgetu32)(FILE *) = NULL;
+
+    if (file_exists(translated_path) != TRUE) {
+        return id;
+    }
+
+    translate_file = fopen(translated_path, "r");
+
+    fprintf(stderr, "0x%08X\n", fgetu32_big(translate_file));
+
+    fclose(translate_file);
+
     return id;
 #else
     return gettext(id);
