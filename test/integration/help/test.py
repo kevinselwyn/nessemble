99c8e094dda4fc733fd31cff96c6d3c4b50c74fd
Kevin Selwyn <kevinselwyn@gmail.com>
2017-04-01 18:20:51 -0400

    Automated integration tests

---
+++ test/integration/help/test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding=utf-8
+# pylint: disable=C0103,C0301
+"""help test"""
+
+import os
+import subprocess
+
+def main():
+    """Main function"""
+
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    example = 'help'
+
+    arguments = ['python', '%s%s..%sintegration-test.py' % (dirname, os.sep, os.sep), example, '--help']
+    child = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    data = child.communicate()[0]
+    rc = child.returncode
+
+    if rc != 0:
+        print data
+
+    exit(rc)
+
+if __name__ == '__main__':
+    main()
