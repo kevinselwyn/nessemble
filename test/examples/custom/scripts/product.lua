01a7994f0a87eac974b29cf30bb013285c90285f
Kevin Selwyn <kevinselwyn@gmail.com>
2017-04-08 18:46:46 -0400

    liblua support

--- test/examples/custom/scripts/product.lua
+++ test/examples/custom/scripts/product.lua
@@ -1,19 +1,15 @@
-#!/usr/bin/env lua
-
-function main(args)
+function custom(...)
     local product = 0
 
-    for i=1,#args
+    for i=1,#arg
     do
         if i == 1
         then
-            product = tonumber(args[i])
+            product = tonumber(arg[i])
         else
-            product = product * tonumber(args[i])
+            product = product * tonumber(arg[i])
         end
     end
 
-    io.write(string.char(product % 256))
+    return string.char(product % 256)
 end
-
-main(arg)
