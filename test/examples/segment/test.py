58e99529bf12dbacfb32712080a309e4efed6092
Kevin Selwyn <kevinselwyn@gmail.com>
2017-05-18 11:09:29 -0400

    Segment test

---
+++ test/examples/segment/test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# coding=utf-8
+# pylint: disable=C0103,C0301
+"""segment test"""
+
+import os
+import subprocess
+
+def main():
+    """Main function"""
+
+    dirname = os.path.dirname(os.path.realpath(__file__))
+    example = 'segment'
+
+    arguments = ['python', '%s%s..%sexamples-test.py' % (dirname, os.sep, os.sep), example]
+    child = subprocess.Popen(arguments, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    data = child.communicate()[0]
+    rc = child.returncode
+
+    if rc != 0:
+        print data
+
+    exit(rc)
+
+if __name__ == '__main__':
+    main()
