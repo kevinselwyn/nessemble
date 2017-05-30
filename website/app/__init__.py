aa946c96976658193e9c481d658c8fd9d999e99d
Kevin Selwyn <kevinselwyn@gmail.com>
2017-05-30 16:35:18 -0400

    API app updates

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -4,6 +4,12 @@
 
 import os
 from flask import Flask, make_response, render_template
+from flask_caching import Cache
+
+#----------------#
+# Constants
+
+CACHE_TIME = 60 * 60
 
 #--------------#
 # Variables
@@ -11,6 +17,10 @@ from flask import Flask, make_response, render_template
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'templates')
 static_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'static')
 app = Flask(__name__, template_folder=tmpl_dir, static_folder=static_dir)
+cache = Cache(app, config={
+    'CACHE_TYPE': 'simple',
+    'CACHE_THRESHOLD': 256
+})
 
 #--------------#
 # Functions
@@ -29,6 +39,7 @@ def custom_response(data, status=200, mimetype='text/html'):
 # Endpoints
 
 @app.route('/', methods=['GET'])
+@cache.cached(timeout=CACHE_TIME)
 def index():
     """Index endpoint"""
 
