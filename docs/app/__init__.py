cf46ac66b294f3f310411477b5c9fa4e2dc72939
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2017-05-30 16:35:18 -0400

    API app updates

--- docs/app/__init__.py
+++ docs/app/__init__.py
@@ -4,8 +4,24 @@
 
 import os
 from flask import Flask, send_from_directory
+from flask_caching import Cache
+
+#----------------#
+# Constants
+
+CACHE_TIME = 60 * 60
+
+#----------------#
+# Variables
 
 app = Flask(__name__)
+cache = Cache(app, config={
+    'CACHE_TYPE': 'simple',
+    'CACHE_THRESHOLD': 256
+})
+
+#----------------#
+# Errors
 
 @app.errorhandler(404)
 def not_found(_error):
@@ -13,31 +29,39 @@ def not_found(_error):
 
     return '404: Not Found', 404
 
+#----------------#
+# Endpoints
+
 @app.route('/')
+@cache.cached(timeout=CACHE_TIME)
 def serve_root():
     """Serve root"""
 
     return serve_path('index.html')
 
 @app.route('/favicon.ico')
+@cache.cached(timeout=CACHE_TIME)
 def serve_favicon():
     """Serve favicon"""
 
     return serve_path(os.path.join('img', 'favicon.ico'))
 
 @app.route('/search.html')
+@cache.cached(timeout=CACHE_TIME)
 def serve_search():
     """Serve search"""
 
     return serve_path('search.html')
 
 @app.route('/sitemap.xml')
+@cache.cached(timeout=CACHE_TIME)
 def serve_sitemap():
     """Serve search"""
 
     return serve_path('sitemap.xml')
 
 @app.route('/<path:path>')
+@cache.cached(timeout=CACHE_TIME)
 def serve_path(path='index.html'):
     """Serve path"""
 
@@ -56,5 +80,8 @@ def serve_path(path='index.html'):
         filename = os.path.join(filename, path)
 
     return send_from_directory(filename, 'index.html')
+
+#--------------#
+# Variables
 
 __all__ = ['app']
