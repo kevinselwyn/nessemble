625b8cc4dc9ef4cb8ec2ade9285822ae6d6e37c9
Kevin Selwyn <kevinselwyn@gmail.com>
2017-06-02 21:36:56 -0400

    Reset password workflow

--- registry/models/users.py
+++ registry/models/users.py
@@ -18,7 +18,22 @@ class User(Base):
     date_login = Column(DateTime)
     login_token = Column(String(128))
     reset_token = Column(String(128))
-    reset_date = Column(DateTime)
+    date_reset = Column(DateTime)
 
     def __repr__(self):
         return "<User(id='%s', name='%s', email='%s', password='%s', date_created='%s', date_login='%s', login_token='%s')>" % (self.id, self.name, self.email, self.password, self.date_created, self.date_login, self.login_token)
+
+    def __getitem__(self, key):
+        switcher = {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'password': self.password,
+            'date_created': self.date_created,
+            'date_login': self.date_login,
+            'login_token': self.login_token,
+            'reset_token': self.reset_token,
+            'date_reset': self.date_reset
+        }
+
+        return switcher.get(key, lambda: None)
