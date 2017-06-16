4844a1bf45e5818fdae52d938b16a88d1d90b416
Kevin Selwyn <kevinselwyn@gmail.com>
2017-06-16 11:08:01 -0400

    Splint src/user.c

--- src/user.c
+++ src/user.c
@@ -5,7 +5,7 @@
 
 unsigned int user_auth(http_t *request, char *token, char *method, char *route) {
     unsigned int rc = RETURN_OK;
-    size_t length = 0;
+    size_t data_length = 0, credentials_length = 0, authorization_length = 0;
     char *username = NULL, *token_hash = NULL, *base64 = NULL;
     char *credentials = NULL, *authorization = NULL, *data = NULL;
     http_t local_request;
@@ -17,25 +17,50 @@ unsigned int user_auth(http_t *request, char *token, char *method, char *route) 
         goto cleanup;
     }
 
-    length = strlen(method) + strlen(route) + 1;
-    data = (char *)nessemble_malloc(sizeof(char) * (length + 1));
-    sprintf(data, "%s+%s", method, route);
+    if (!username) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
+    data_length = strlen(method) + strlen(route) + 1;
+    data = (char *)nessemble_malloc(sizeof(char) * (data_length + 1));
+
+    if (snprintf(data, data_length+1, "%s+%s", method, route) != (int)data_length) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
     hmac(&token_hash, token, strlen(token), data, strlen(data));
 
-    length = strlen(username) + strlen(token_hash) + 1;
-    credentials = (char *)nessemble_malloc(sizeof(char) * (length + 1));
+    if (!token_hash) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
-    sprintf(credentials, "%s:%s", username, token_hash);
+    credentials_length = strlen(username) + strlen(token_hash) + 1;
+    credentials = (char *)nessemble_malloc(sizeof(char) * (credentials_length + 1));
+
+    if (snprintf(credentials, credentials_length+1, "%s:%s", username, token_hash) != (int)credentials_length) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
     if ((rc = base64enc(&base64, credentials)) != RETURN_OK) {
         goto cleanup;
     }
 
-    length = strlen(base64) + 10;
-    authorization = (char *)nessemble_malloc(sizeof(char) * (length + 1));
+    if (!base64) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
-    sprintf(authorization, "HMAC-SHA1 %s", base64);
+    authorization_length = strlen(base64) + 10;
+    authorization = (char *)nessemble_malloc(sizeof(char) * (authorization_length + 1));
+
+    if (snprintf(authorization, authorization_length+1, "HMAC-SHA1 %s", base64) != (int)authorization_length) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
     if ((rc = http_header(&local_request, "Authorization", authorization)) != RETURN_OK) {
         goto cleanup;
@@ -56,13 +81,13 @@ cleanup:
 
 unsigned int user_create() {
     unsigned int rc = RETURN_OK;
-    size_t length = 0;
+    size_t length = 0, data_length = 0;
     char *url = NULL, *error = NULL, *buffer = NULL;
     char *user_name = NULL, *user_username = NULL, *user_email = NULL, *user_password = NULL;
     char data[1024];
     http_t request;
 
-    memset(data, '\0', 1024);
+    memset(data, 0, 1024);
     buffer = (char *)nessemble_malloc(sizeof(char) * BUF_GET_LINE);
 
     while (get_line(&buffer, _("Name: ")) != NULL) {
@@ -77,6 +102,11 @@ unsigned int user_create() {
         break;
     }
 
+    if (!user_name) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
     while (get_line(&buffer, _("Username: ")) != NULL) {
         length = strlen(buffer);
 
@@ -87,6 +117,11 @@ unsigned int user_create() {
         buffer[length - 1] = '\0';
         user_username = nessemble_strdup(buffer);
         break;
+    }
+
+    if (!user_username) {
+        rc = RETURN_EPERM;
+        goto cleanup;
     }
 
     while (get_line(&buffer, _("Email: ")) != NULL) {
@@ -101,6 +136,11 @@ unsigned int user_create() {
         break;
     }
 
+    if (!user_email) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
     while ((user_password = nessemble_getpass(_("Password: "))) != NULL) {
         length = strlen(buffer);
 
@@ -111,9 +151,26 @@ unsigned int user_create() {
         break;
     }
 
-    sprintf(data, "{\n\t\"name\":\"%s\",\n\t\"username\":\"%s\",\n\t\"email\":\"%s\",\n\t\"password\":\"%s\"\n}", user_name, user_username, user_email, user_password);
+    if (!user_password) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
+    data_length = strlen(user_name) + strlen(user_username) + strlen(user_email) + strlen(user_password) + 59;
+
+    if (snprintf(data, data_length+1, "{\n\t\"name\":\"%s\",\n\t\"username\":\"%s\",\n\t\"email\":\"%s\",\n\t\"password\":\"%s\"\n}", user_name, user_username, user_email, user_password) != (int)data_length) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
+    fprintf(stderr, "%s\n", data);
 
     if ((rc = api_user(&url, "create")) != RETURN_OK) {
+        goto cleanup;
+    }
+
+    if (!url) {
+        rc = RETURN_EPERM;
         goto cleanup;
     }
 
@@ -140,14 +197,16 @@ unsigned int user_create() {
         if ((rc = get_json_buffer(&error, "error", request.response_body)) != RETURN_OK) {
             error_program_log(_("Could not read response"));
         } else {
-            error_program_log(error);
+            if (error) {
+                error_program_log(error);
+            }
         }
 
         rc = RETURN_EPERM;
         goto cleanup;
     }
 
-    if (!request.content_length) {
+    if (request.content_length == 0) {
         rc = RETURN_EPERM;
         goto cleanup;
     }
@@ -171,14 +230,14 @@ cleanup:
 
 unsigned int user_login() {
     unsigned int rc = RETURN_OK;
-    size_t length = 0;
+    size_t length = 0, data_length = 0, auth_length = 0;
     char *url = NULL, *error = NULL, *buffer = NULL, *token = NULL;
     char *base64 = NULL, *auth = NULL;
     char *user_username = NULL, *user_password = NULL;
     char data[1024];
     http_t request;
 
-    memset(data, '\0', 1024);
+    memset(data, 0, 1024);
     buffer = (char *)nessemble_malloc(sizeof(char) * BUF_GET_LINE);
     auth = (char *)nessemble_malloc(sizeof(char) * 1024);
 
@@ -194,6 +253,11 @@ unsigned int user_login() {
         break;
     }
 
+    if (!user_username) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
     while ((user_password = nessemble_getpass(_("Password: "))) != NULL) {
         length = strlen(buffer);
 
@@ -204,15 +268,40 @@ unsigned int user_login() {
         break;
     }
 
-    sprintf(data, "%s:%s", user_username, user_password);
+    if (!user_password) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
+    data_length = strlen(user_username) + strlen(user_password) + 1;
+
+    if (snprintf(data, data_length+1, "%s:%s", user_username, user_password) != (int)data_length) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
     if ((rc = base64enc(&base64, data)) != RETURN_OK) {
         goto cleanup;
     }
 
-    sprintf(auth, "Basic %s", base64);
+    if (!base64) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
+    auth_length = strlen(base64) + 6;
+
+    if (snprintf(auth, auth_length+1, "Basic %s", base64) != (int)auth_length) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
     if ((rc = api_user(&url, "login")) != RETURN_OK) {
+        goto cleanup;
+    }
+
+    if (!url) {
+        rc = RETURN_EPERM;
         goto cleanup;
     }
 
@@ -235,7 +324,7 @@ unsigned int user_login() {
         goto cleanup;
     }
 
-    if (!request.content_length) {
+    if (request.content_length == 0) {
         rc = RETURN_EPERM;
         goto cleanup;
     }
@@ -244,7 +333,9 @@ unsigned int user_login() {
         if ((rc = get_json_buffer(&error, "error", request.response_body)) != RETURN_OK) {
             error_program_log(_("Could not read response"));
         } else {
-            error_program_log(error);
+            if (error) {
+                error_program_log(error);
+            }
         }
 
         rc = RETURN_EPERM;
@@ -252,6 +343,11 @@ unsigned int user_login() {
     }
 
     if ((rc = get_json_buffer(&token, "token", request.response_body)) != RETURN_OK) {
+        goto cleanup;
+    }
+
+    if (!token) {
+        rc = RETURN_EPERM;
         goto cleanup;
     }
 
@@ -285,7 +381,17 @@ unsigned int user_logout() {
         goto cleanup;
     }
 
+    if (!token) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
     if ((rc = api_user(&url, "logout")) != RETURN_OK) {
+        goto cleanup;
+    }
+
+    if (!url) {
+        rc = RETURN_EPERM;
         goto cleanup;
     }
 
@@ -312,7 +418,7 @@ unsigned int user_logout() {
         goto cleanup;
     }
 
-    if (!request.content_length) {
+    if (request.content_length == 0) {
         rc = RETURN_EPERM;
         goto cleanup;
     }
@@ -321,7 +427,9 @@ unsigned int user_logout() {
         if ((rc = get_json_buffer(&error, "error", request.response_body)) != RETURN_OK) {
             error_program_log(_("Could not read response"));
         } else {
-            error_program_log(error);
+            if (error) {
+                error_program_log(error);
+            }
         }
 
         rc = RETURN_EPERM;
@@ -344,14 +452,14 @@ cleanup:
 
 unsigned int user_forgotpassword() {
     unsigned int rc = RETURN_OK;
-    size_t length = 0;
+    size_t length = 0, data_length = 0;
     char *url = NULL, *error = NULL, *buffer = NULL;
     char *user_username = NULL;
     char *json_email = NULL, *json_url = NULL;
     char data[1024];
     http_t request;
 
-    memset(data, '\0', 1024);
+    memset(data, 0, 1024);
     buffer = (char *)nessemble_malloc(sizeof(char) * BUF_GET_LINE);
 
     while (get_line(&buffer, _("Username: ")) != NULL) {
@@ -366,9 +474,24 @@ unsigned int user_forgotpassword() {
         break;
     }
 
-    sprintf(data, "{\n\t\"username\":\"%s\"\n}", user_username);
+    if (!user_username) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
+    data_length = strlen(user_username) + 18;
+
+    if (snprintf(data, data_length+1, "{\n\t\"username\":\"%s\"\n}", user_username) != (int)data_length) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
     if ((rc = api_user(&url, "forgotpassword")) != RETURN_OK) {
+        goto cleanup;
+    }
+
+    if (!url) {
+        rc = RETURN_EPERM;
         goto cleanup;
     }
 
@@ -391,7 +514,7 @@ unsigned int user_forgotpassword() {
         goto cleanup;
     }
 
-    if (!request.content_length) {
+    if (request.content_length == 0) {
         rc = RETURN_EPERM;
         goto cleanup;
     }
@@ -400,7 +523,9 @@ unsigned int user_forgotpassword() {
         if ((rc = get_json_buffer(&error, "error", request.response_body)) != RETURN_OK) {
             error_program_log(_("Could not read response"));
         } else {
-            error_program_log(error);
+            if (error) {
+                error_program_log(error);
+            }
         }
 
         rc = RETURN_EPERM;
@@ -411,8 +536,18 @@ unsigned int user_forgotpassword() {
         goto cleanup;
     }
 
+    if (!json_email) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
     if (strcmp(json_email, "false") == 0) {
         if ((rc = get_json_buffer(&json_url, "url", request.response_body)) != RETURN_OK) {
+            goto cleanup;
+        }
+
+        if (!json_url) {
+            rc = RETURN_EPERM;
             goto cleanup;
         }
 
@@ -435,13 +570,13 @@ cleanup:
 
 unsigned int user_resetpassword() {
     unsigned int rc = RETURN_OK;
-    size_t length = 0;
+    size_t length = 0, data_length = 0;
     char *url = NULL, *error = NULL, *buffer = NULL;
     char *user_token = NULL, *user_username = NULL, *user_password = NULL;
     char data[1024];
     http_t request;
 
-    memset(data, '\0', 1024);
+    memset(data, 0, 1024);
     buffer = (char *)nessemble_malloc(sizeof(char) * BUF_GET_LINE);
 
     while (get_line(&buffer, _("Token: ")) != NULL) {
@@ -456,6 +591,11 @@ unsigned int user_resetpassword() {
         break;
     }
 
+    if (!user_token) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
     while (get_line(&buffer, _("Username: ")) != NULL) {
         length = strlen(buffer);
 
@@ -468,6 +608,11 @@ unsigned int user_resetpassword() {
         break;
     }
 
+    if (!user_username) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
+
     while ((user_password = nessemble_getpass(_("Password: "))) != NULL) {
         length = strlen(buffer);
 
@@ -476,6 +621,11 @@ unsigned int user_resetpassword() {
         }
 
         break;
+    }
+
+    if (!user_password) {
+        rc = RETURN_EPERM;
+        goto cleanup;
     }
 
     if ((rc = set_config(user_username, "username")) != RETURN_OK) {
@@ -488,9 +638,19 @@ unsigned int user_resetpassword() {
         goto cleanup;
     }
 
-    sprintf(data, "{\n\t\"username\":\"%s\",\n\t\"password\":\"%s\"\n}", user_username, user_password);
+    data_length = strlen(user_username) + strlen(user_password) + 34;
+
+    if (snprintf(data, data_length+1, "{\n\t\"username\":\"%s\",\n\t\"password\":\"%s\"\n}", user_username, user_password) != (int)data_length) {
+        rc = RETURN_EPERM;
+        goto cleanup;
+    }
 
     if ((rc = api_user(&url, "resetpassword")) != RETURN_OK) {
+        goto cleanup;
+    }
+
+    if (!url) {
+        rc = RETURN_EPERM;
         goto cleanup;
     }
 
@@ -511,7 +671,7 @@ unsigned int user_resetpassword() {
         goto cleanup;
     }
 
-    if (!request.content_length) {
+    if (request.content_length == 0) {
         rc = RETURN_EPERM;
         goto cleanup;
     }
@@ -520,7 +680,9 @@ unsigned int user_resetpassword() {
         if ((rc = get_json_buffer(&error, "error", request.response_body)) != RETURN_OK) {
             error_program_log(_("Could not read response"));
         } else {
-            error_program_log(error);
+            if (error) {
+                error_program_log(error);
+            }
         }
 
         rc = RETURN_EPERM;
