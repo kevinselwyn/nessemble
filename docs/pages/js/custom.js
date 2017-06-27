5c7e0d546ad99cf019732066408e064cfff3c117
Kevin Selwyn <kevin.selwyn@ampermusic.com>
2017-06-27 11:19:56 -0400

    Module hexdump

--- docs/pages/js/custom.js
+++ docs/pages/js/custom.js
@@ -38,6 +38,95 @@ var Nessemble = function (opts) {
     });
 };
 
+var hexdump = function (bytes) {
+    var dump = [],
+        dec2hex = function (dec, len) {
+            var hex = dec.toString(16).toLowerCase();
+
+            while (hex.length < len) {
+                hex = '0' + hex;
+            }
+
+            return hex;
+        },
+        index = 0,
+        lines = [],
+        line = {},
+        chr = '',
+        i = 0,
+        j = 0,
+        k = 0,
+        l = 0;
+
+    for (i = 0, j = bytes.length; i < j; i += 16) {
+        line = {
+            index: dec2hex(i, 8),
+            bytes: [[], []],
+            text: []
+        };
+
+        for (k = i, l = i + 8; k < l; k += 1) {
+            if (k >= j) {
+                break;
+            }
+
+            index += 1;
+            line.bytes[0].push(dec2hex(bytes[k], 2));
+
+            if (bytes[k] < 0x20 || bytes[k] > 0x7E) {
+                chr = '.';
+            } else {
+                chr = String.fromCharCode(bytes[k]);
+            }
+
+            line.text.push(chr);
+        }
+
+        for (k = i + 8, l = i + 16; k < l; k += 1) {
+            if (k >= j) {
+                break;
+            }
+
+            index += 1;
+            line.bytes[1].push(dec2hex(bytes[k], 2));
+
+            if (bytes[k] < 0x20 || bytes[k] > 0x7E) {
+                chr = '.';
+            } else {
+                chr = String.fromCharCode(bytes[k]);
+            }
+
+            line.text.push(chr);
+        }
+
+        lines.push(line);
+    }
+
+    lines.push({
+        index: dec2hex(index, 8),
+        bytes: [[], []],
+        text: []
+    })
+
+    lines.forEach(function (line) {
+        var str = '';
+
+        str = [line.index, line.bytes[0].join(' '), line.bytes[1].join(' ')].join('  ');
+
+        while (str.length < 60) {
+            str += ' ';
+        }
+
+        if (line.text.length) {
+            str += ['|', line.text.join(''), '|'].join('');
+        }
+
+        dump.push(str);
+    });
+
+    return dump.join('\n');
+};
+
 (function () {
     var examples = document.querySelectorAll('.nessemble-example');
 
@@ -50,7 +139,12 @@ var Nessemble = function (opts) {
             input = document.createElement('textarea'),
             assemble = document.createElement('input'),
             output_wrapper = document.createElement('pre'),
-            output = document.createElement('code');
+            output = document.createElement('code'),
+            element = document.createElement('div');
+
+        // decode html entities
+        element.innerHTML = stdin;
+        stdin = element.textContent;
 
         assemble.addEventListener('click', function () {
             var nessemble = new Nessemble({
@@ -61,8 +155,15 @@ var Nessemble = function (opts) {
 
                     output.innerHTML = '';
 
-                    for (i = 0, l = stdout.length; i < l; i += 1) {
-                        output.innerHTML += '0x' + stdout[i].toString(16) + ' ';
+                    if (!stdout.length) {
+                        output.className = output.className.replace(' show', '');
+                        return;
+                    }
+
+                    output.innerHTML = hexdump(stdout);
+
+                    if (output.className.search(' show') === -1) {
+                        output.className += ' show';
                     }
                 }
             });
@@ -79,13 +180,18 @@ var Nessemble = function (opts) {
         // setup button
         assemble.type = 'button';
         assemble.value = 'Assemble';
+        assemble.className = 'btn btn-danger';
 
         // setup output
+        output.className = 'text';
         output_wrapper.appendChild(output);
 
         // append all
         example.appendChild(input);
         example.appendChild(assemble);
-        example.appendChild(output);
+        example.appendChild(output_wrapper);
+
+        // show module
+        example.className += ' show';
     });
 }());
