3a35b5f798c72450e45fefe68bc523bd7a0ed3bc
Kevin Selwyn <kevinselwyn@gmail.com>
2017-07-20 13:58:33 -0400

    SLO + SRE

--- src/simulate/illegal.c
+++ src/simulate/illegal.c
@@ -277,11 +277,39 @@ void do_rra(unsigned int opcode_index, unsigned int value) {
 }
 
 void do_slo(unsigned int opcode_index, unsigned int value) {
-    /* TODO: Undocumented */
+    unsigned int tmp = 0, address = 0;
+
+    address = get_address(opcode_index, value);
+    tmp = get_byte(address);
+
+    tmp <<= 1;
+
+    set_register(REGISTER_A, (tmp | get_register(REGISTER_A)) & 0xFF);
+
+    set_flag(FLG_NEGATIVE, (tmp >> 7) & 1);
+    set_flag(FLG_CARRY, (tmp >> 7) & 1);
+    set_flag(FLG_ZERO, (unsigned int)(tmp == 0 ? TRUE : FALSE));
+
+    inc_register(REGISTER_PC, (int)opcodes[opcode_index].length);
+    inc_cycles(opcodes[opcode_index].timing);
 }
 
 void do_sre(unsigned int opcode_index, unsigned int value) {
-    /* TODO: Undocumented */
+    unsigned int tmp = 0, address = 0;
+
+    address = get_address(opcode_index, value);
+    tmp = get_byte(address);
+
+    tmp >>= 1;
+
+    set_register(REGISTER_A, (tmp ^ get_register(REGISTER_A)) & 0xFF);
+
+    set_flag(FLG_NEGATIVE, (tmp >> 7) & 1);
+    set_flag(FLG_CARRY, (tmp >> 7) & 1);
+    set_flag(FLG_ZERO, (unsigned int)(tmp == 0 ? TRUE : FALSE));
+
+    inc_register(REGISTER_PC, (int)opcodes[opcode_index].length);
+    inc_cycles(opcodes[opcode_index].timing);
 }
 
 void do_sxa(unsigned int opcode_index, unsigned int value) {
